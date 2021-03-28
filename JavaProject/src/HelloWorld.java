af8fc247774a9b64c6b6561810edac1922024181
sagarneupane <sagarneupane@macbook-pro.lan>
2021-03-28 22:11:41 +1100

    display method is added...

--- JavaProject/src/HelloWorld.java
+++ JavaProject/src/HelloWorld.java
@@ -2,7 +2,12 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-System.out.println("Hello World...");
+		System.out.println("Hello World...");
+	}
+
+	public static void display() {
+		System.out.println("display method...");
+
 	}
 
 }
